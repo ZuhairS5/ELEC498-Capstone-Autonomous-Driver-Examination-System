e724c51d7862e1928b4ce71616ea7500ce8b2a05
JoshuaArgiropoulos <joshua.argiropoulos@gmail.com>
2024-01-10 15:04:21 -0500

    Created frontend

---
+++ website/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Homepage from './MainPages/Homepage';
+
+function App() {
+  return (
+    
+    <div className="App">
+      
+      
+      <Homepage/>
+    </div>
+  );
+}
+
+export default App;
