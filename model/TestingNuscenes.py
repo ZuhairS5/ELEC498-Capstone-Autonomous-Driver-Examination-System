aab2bed9eb5e138553bd6a9f67ccb4a8d4a4c624
JoshuaArgiropoulos <joshua.argiropoulos@gmail.com>
2024-02-02 17:26:13 -0500

    working with nuimages

--- model/TestingNuscenes.py
+++ model/TestingNuscenes.py
@@ -1,15 +1,4 @@
-from nuscenes.nuscenes import NuScenes
+from nuimages import NuImages
 
-# Initialize a 'NuScenes' object
-nusc = NuScenes(version='v1.0-mini', dataroot='./data/nuscenes/', verbose=True)
-
-# Print the first record to check if it's loaded correctly
-print(nusc.sample[0])
-
-my_sample = nusc.sample[0]
-nusc.render_sample_data(my_sample['data']['CAM_FRONT'])
-
-# Get the annotated data
-my_annotation_token = my_sample['anns'][0]
-my_annotation_data = nusc.get('sample_annotation', my_annotation_token)
-print(my_annotation_data)
+# If 'MODEL' is your current working directory when executing the script
+nuim = NuImages(dataroot='./data/sets/nuimages', version='v1.0-mini', verbose=True, lazy=True)
