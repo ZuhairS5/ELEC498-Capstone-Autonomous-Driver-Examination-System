0fb725b56978b3ceaa43af14552a654dc16f6f0f
evanmelrose <evan.melrose4@gmail.com>
2024-03-06 18:23:17 -0500

    Reduced model size

--- model/model.py
+++ model/model.py
@@ -6,9 +6,8 @@ class model (nn.Module):
     def __init__(self):
         super(model, self).__init__()
         self.resNext = torchvision.models.resnet50(weights = None)
-        self.fc1 = nn.Linear(1000,100)
-        self.fc2= nn.Linear(100, 10)
-        self.fc3 = nn.Linear(10,2)
+        self.fc1 = nn.Linear(1000,10)
+        self.fc2= nn.Linear(10, 2)
         self.relu = nn.ReLU()
         self.sig = nn.Sigmoid()
         self.softmax = nn.Softmax(dim=1)
@@ -19,8 +18,6 @@ class model (nn.Module):
         x = self.fc1(x)
         x = self.relu(x)
         x = self.fc2(x)
-        x = self.relu(x)
-        x = self.fc3(x)
         x = self.sig(x)
         x = self.softmax(x)
         return x
